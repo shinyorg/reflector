6c67c6e24ed8096933d8d93dcfcb82509f9eaabe
Allan Ritchie <allan.ritchie@gmail.com>
2025-06-27 00:11:13 -0400

    TODO

--- Shiny.Reflector/IReflectorClass.cs
+++ Shiny.Reflector/IReflectorClass.cs
@@ -11,3 +11,21 @@ public interface IReflectorClass
         
     object? this[string key] { get; set; }
 }
+
+//void CallMethod(string methodName, params object?[]? parameters);
+// object? CallMethod
+//Task CallAsyncMethod
+// Task<T> CallAsyncMethod<T>(string methodName, params object?[]? parameters);
+
+// public record MethodGeneratedInfo(
+//     string Name,
+//     Type ReturnType,
+//     ParameterGeneratedInfo[] Parameters
+// );
+//
+// public record ParameterGeneratedInfo(
+//     string Name,
+//     Type Type,
+//     bool IsOptional = false,
+//     object? DefaultValue = null
+// );
